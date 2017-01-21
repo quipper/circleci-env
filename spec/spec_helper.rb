0a6c139cb035d4f33d39026b443c71690447c1c1
Kazuyuki Honda <hakobera@gmail.com>
2017-01-21 23:49:06 +0900

    Collect coverage using simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,9 @@
+require 'simplecov'
+SimpleCov.start do
+  add_filter "/.bundle/"
+  add_filter "/examples/"
+  add_filter "/spec/"
+end
+
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "circleci/env"
